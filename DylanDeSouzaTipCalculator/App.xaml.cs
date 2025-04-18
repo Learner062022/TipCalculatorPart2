221a6a87c9d32365521d99062eb2bce0d2b7d58c
Learner062022 <ddylan246@yahoo.com>
2025-04-18 18:08:51 +0800

    feat(app): initialize Syncfusion license and enhance app lifecycle handling
    - Registered Syncfusion license in the  class for slider functionality.
    - Initialized  instance for managing preferences.
    - Configured theme settings on application startup using stored preferences.
    - Implemented  method to initialize .

--- DylanDeSouzaTipCalculator/App.xaml.cs
+++ DylanDeSouzaTipCalculator/App.xaml.cs
@@ -4,16 +4,20 @@ namespace DylanDeSouzaTipCalculator
 {
     public partial class App : Application
     {
+        Model model = new();
+
         public App()
         {
             SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF1cXmhKYVFzWmFZfVtgdVdMZFxbRXZPMyBoS35Rc0VhW3xed3VSRWheV0F+");
             InitializeComponent();
-
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
+        protected override Window CreateWindow(IActivationState? activationState) => new(new AppShell());
+
+        protected override void OnStart()
         {
-            return new Window(new AppShell());
+            model.ThemeIsToggled = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(model.ThemeIsToggled), false);
+            Current.UserAppTheme = model.ThemeIsToggled ? AppTheme.Dark : AppTheme.Light;
         }
     }
 }
