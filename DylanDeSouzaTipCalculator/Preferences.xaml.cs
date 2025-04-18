daec5fad93d798e662d9f26e3469eaf6a0def49b
Learner062022 <ddylan246@yahoo.com>
2025-04-18 17:57:40 +0800

    refactor(preferences): simplify Preferences class and enhance binding logic
    - Made 'model' readonly to ensure better immutability.
    - Removed unused properties ('ThemeIsToggled', 'FontSize', 'PlaySound', 'Colors', 'ColorScheme') for improved maintainability.
    - Refactored 'OnPropertyChanged' logic to streamline code structure.
    - Optimized 'BindingContext' assignment for better data binding clarity.

--- DylanDeSouzaTipCalculator/Preferences.xaml.cs
+++ DylanDeSouzaTipCalculator/Preferences.xaml.cs
@@ -1,10 +1,8 @@
-using System.Runtime.CompilerServices;
-
 namespace DylanDeSouzaTipCalculator
 {
     public partial class Preferences : ContentPage
     {
-        Model model;
+        private readonly Model model;
 
         public Preferences(Model model)
         {
@@ -12,43 +10,6 @@ namespace DylanDeSouzaTipCalculator
             this.model = model;
             model.LoadPreferencesPageSettings();
             BindingContext = this.model;
-
-        }
-
-        protected override void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
-            base.OnPropertyChanged(propertyName);
-
-        public bool ThemeIsToggled
-        {
-            get => model.ThemeIsToggled;
-            set { if (model.ThemeIsToggled != value) model.ThemeIsToggled = value; }
-        }
-
-        public double FontSize
-        {
-            get => model.FontSize;
-            set 
-            { 
-                if (model.FontSize != value)
-                {
-                    model.FontSize = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public bool PlaySound
-        {
-            get => model.PlaySound;
-            set { if (model.PlaySound != value) model.PlaySound = value; }
-        }
-
-        public List<string> Colors { get => model.Colors; }
-
-        public string? ColorScheme
-        {
-            get => model.ColorScheme;
-            set { if (model.ColorScheme != value) model.ColorScheme = value; }
         }
 
         protected override bool OnBackButtonPressed()
