89781e522683d37ff0b0d665712c944a551df5ae
Learner062022 <ddylan246@yahoo.com>
2025-04-18 17:10:48 +0800

    feat(tip-calculator): add model class for tip calculation and settings management
    Introduced a  class that implements the  interface. This class enables functionalities such as calculating tips, managing user preferences, handling numeric and decimal inputs, playing sound effects and toggling themes. It also handles property updates and ensures dependent properties are notified when changes occur.

--- DylanDeSouzaTipCalculator/Model.cs
+++ DylanDeSouzaTipCalculator/Model.cs
@@ -11,56 +11,16 @@ namespace DylanDeSouzaTipCalculator
         string billAmount = "0";
         int amountDiners;
         double tipPercentage;
-        int amountDecimalNumbers;
+        bool themeIsToggled;
+        string? colorChoice;
+        readonly List<string> colorChoices = ["None", "Red", "Green", "Blue"];
+        Color? selectedBackgroundColor;
         IAudioPlayer? buttonSound;
         bool decimalButtonPressed;
+        int amountDecimalNumbers;
         bool playSound;
         double fontSize;
-        bool themeIsToggled;
         string? currentPageOn;
-        List<string> colors = ["Red", "Green", "Blue"];
-        string? colorScheme;
-
-        public string? CurrentPageOn
-        {
-            get => currentPageOn;
-            set
-            {
-                currentPageOn = value;
-                Microsoft.Maui.Storage.Preferences.Default.Set(nameof(CurrentPageOn), currentPageOn);
-                OnPropertyChanged();
-            }
-        }
-
-        public bool ThemeIsToggled
-        {
-            get => themeIsToggled;
-            set
-            {
-                if (themeIsToggled != value)
-                {
-                    themeIsToggled = value;
-                    OnPropertyChanged();
-                    Microsoft.Maui.Storage.Preferences.Default.Set(nameof(ThemeIsToggled), themeIsToggled);
-                }
-            }
-        }
-
-        public List<string> Colors { get => colors; }
-
-        public string? ColorScheme
-        {
-            get => colorScheme;
-            set
-            {
-                if (colorScheme != value)
-                {
-                    colorScheme = value;
-                    OnPropertyChanged();
-                    Microsoft.Maui.Storage.Preferences.Default.Set(nameof(ColorScheme), colorScheme);
-                }
-            }
-        }
 
         public string BillAmount
         {
@@ -71,31 +31,107 @@ namespace DylanDeSouzaTipCalculator
                 {
                     billAmount = value;
                     OnPropertyChanged();
-                    Microsoft.Maui.Storage.Preferences.Default.Set(nameof(BillAmount), billAmount);
                     NotifyDependentProperties(nameof(TipAmount), nameof(Total), nameof(SplitAmount));
                 }
             }
         }
 
-        public void HandleNumericInput(string value)
+        public int AmountDiners
         {
-            if (DecimalButtonPressed && AmountDecimalNumbers >= 2) return;
-
-            if (DecimalButtonPressed) AmountDecimalNumbers++;
-
-            BillAmount = BillAmount == "0" ? value : BillAmount + value;
-        }
-
-        public double FontSize
-        {
-            get => fontSize;
+            get => amountDiners;
             set
             {
-                if (fontSize != value)
+                if (amountDiners != value)
                 {
-                    fontSize = value;
+                    amountDiners = value;
                     OnPropertyChanged();
-                    Microsoft.Maui.Storage.Preferences.Default.Set(nameof(FontSize), fontSize);
+                    NotifyDependentProperties(nameof(SplitAmount));
+                }
+            }
+        }
+
+        public double TipPercentage
+        {
+            get => tipPercentage;
+            set
+            {
+                tipPercentage = value;
+                OnPropertyChanged();
+                NotifyDependentProperties(nameof(TipAmount), nameof(Total), nameof(SplitAmount));
+            }
+        }
+
+        public double TipAmount => TipPercentage * double.Parse(BillAmount);
+
+        public double Total => TipAmount + double.Parse(BillAmount);
+
+        public double SplitAmount => AmountDiners > 0 ? Total / AmountDiners : 0;
+
+        public bool ThemeIsToggled
+        {
+            get => themeIsToggled;
+            set
+            {
+                if (themeIsToggled != value)
+                {
+                    themeIsToggled = value;
+                    SetUserAppTheme(themeIsToggled);
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string? ColorChoice
+        {
+            get => colorChoice;
+            set
+            {
+                if (colorChoice != value)
+                {
+                    colorChoice = value;
+                    Microsoft.Maui.Storage.Preferences.Default.Set(nameof(ColorChoice), colorChoice);
+                    OnPropertyChanged();
+                    UpdateSelectedColor(colorChoice);
+                }
+            }
+        }
+
+        void UpdateSelectedColor(string colorChoice)
+        {
+            SelectedBackgroundColor = colorChoice switch
+            {
+                "Red" => Colors.Red,
+                "Green" => Colors.Green,
+                "Blue" => Colors.Blue,
+                _ => null
+            };
+        }
+
+        public List<string> ColorChoices => colorChoices;
+
+        public Color? SelectedBackgroundColor
+        {
+            get => selectedBackgroundColor;
+            set
+            {
+                if (selectedBackgroundColor != value)
+                {
+                    selectedBackgroundColor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string? CurrentPageOn
+        {
+            get => currentPageOn;
+            set
+            {
+                if (currentPageOn != value)
+                {
+                    currentPageOn = value;
+                    Microsoft.Maui.Storage.Preferences.Default.Set(nameof(CurrentPageOn), currentPageOn);
+                    OnPropertyChanged();
                 }
             }
         }
@@ -111,97 +147,6 @@ namespace DylanDeSouzaTipCalculator
                     OnPropertyChanged();
                 }
             }
-        }
-
-        public int AmountDiners
-        {
-            get => amountDiners;
-            set
-            {
-                if (amountDiners != value)
-                {
-                    amountDiners = value;
-                    OnPropertyChanged();
-                    Microsoft.Maui.Storage.Preferences.Default.Set(nameof(AmountDiners), amountDiners);
-                    NotifyDependentProperties(nameof(SplitAmount));
-                }
-            }
-        }
-
-        public double TipPercentage
-        {
-            get => tipPercentage;
-            set
-            {
-                tipPercentage = value;
-                OnPropertyChanged();
-                Microsoft.Maui.Storage.Preferences.Default.Set(nameof(TipPercentage), tipPercentage);
-                NotifyDependentProperties(nameof(TipAmount), nameof(Total), nameof(SplitAmount));
-            }
-        }
-
-        public double TipAmount => TipPercentage * double.Parse(BillAmount);
-
-        public double Total => TipAmount + double.Parse(BillAmount);
-
-        public double SplitAmount => AmountDiners > 0 ? Total / AmountDiners : 0;
-
-        public void LoadMainPagePreferences()
-        {
-            BillAmount = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(BillAmount), "0");
-            AmountDiners = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(AmountDiners), 1);
-            TipPercentage = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(TipPercentage), 0.0);
-
-            DecimalButtonPressed = BillAmount.Contains('.');
-            AmountDecimalNumbers = DecimalButtonPressed
-                ? BillAmount.Length - (BillAmount.IndexOf('.') + 1)
-                : 0;
-        }
-
-        public void LoadPreferencesPageSettings()
-        {
-            PlaySound = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(PlaySound), false);
-            ThemeIsToggled = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(ThemeIsToggled), false);
-            FontSize = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(FontSize), 0.0);
-            CurrentPageOn = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(CurrentPageOn), "main");
-            ColorScheme = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(ColorScheme), "Red");
-        }
-
-        public async Task InitializeAudioPlayerAsync()
-        {
-            if (buttonSound == null)
-            {
-                Stream soundFile = await FileSystem.OpenAppPackageFileAsync("tap.wav");
-                buttonSound = AudioManager.Current.CreatePlayer(soundFile);
-            }
-        }
-
-        public void PlayButtonSound() => buttonSound?.Play();
-
-        void NotifyDependentProperties(params string[] propertyNames)
-        {
-            foreach (var propertyName in propertyNames) OnPropertyChanged(propertyName);
-        }
-
-        public void Reset()
-        {
-            BillAmount = "0";
-            AmountDiners = 1;
-            AmountDecimalNumbers = 0;
-            DecimalButtonPressed = false;
-
-            NotifyDependentProperties(nameof(BillAmount), nameof(TipAmount), nameof(Total), nameof(SplitAmount));
-        }
-
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-        public void HandleDecimalInput()
-        {
-            if (DecimalButtonPressed) return;
-
-            DecimalButtonPressed = true;
-            BillAmount += ".";
         }
 
         public int AmountDecimalNumbers
@@ -226,9 +171,88 @@ namespace DylanDeSouzaTipCalculator
                 {
                     playSound = value;
                     OnPropertyChanged();
-                    Microsoft.Maui.Storage.Preferences.Default.Set(nameof(PlaySound), playSound);
                 }
             }
         }
+
+        public double FontSize
+        {
+            get => fontSize;
+            set
+            {
+                if (fontSize != value)
+                {
+                    fontSize = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        void SetUserAppTheme(bool isDarkModeEnabled)
+        {
+            Application.Current.UserAppTheme = isDarkModeEnabled ? AppTheme.Dark : AppTheme.Light;
+            OnPropertyChanged(nameof(ThemeIsToggled));
+        }
+
+        void NotifyDependentProperties(params string[] propertyNames)
+        {
+            foreach (var propertyName in propertyNames)
+                OnPropertyChanged(propertyName);
+        }
+
+        public void Reset()
+        {
+            BillAmount = "0";
+            AmountDiners = 1;
+            AmountDecimalNumbers = 0;
+            DecimalButtonPressed = false;
+            NotifyDependentProperties(nameof(BillAmount), nameof(TipAmount), nameof(Total), nameof(SplitAmount));
+        }
+
+        public void HandleNumericInput(string value)
+        {
+            if (DecimalButtonPressed && AmountDecimalNumbers >= 2) return;
+
+            if (DecimalButtonPressed) AmountDecimalNumbers++;
+
+            BillAmount = BillAmount == "0" ? value : BillAmount + value;
+        }
+
+        public void HandleDecimalInput()
+        {
+            if (DecimalButtonPressed) return;
+
+            DecimalButtonPressed = true;
+            BillAmount += ".";
+        }
+
+        public async Task InitializeAudioPlayerAsync() =>
+            buttonSound ??= AudioManager.Current.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("tap.wav"));
+
+        public void PlayButtonSound() => buttonSound?.Play();
+
+        public void LoadMainPagePreferences()
+        {
+            BillAmount = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(BillAmount), "0");
+            AmountDiners = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(AmountDiners), 1);
+            TipPercentage = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(TipPercentage), 0.0);
+            DecimalButtonPressed = BillAmount.Contains('.');
+            AmountDecimalNumbers = DecimalButtonPressed
+                ? BillAmount.Length - (BillAmount.IndexOf('.') + 1)
+                : 0;
+        }
+
+        public void LoadPreferencesPageSettings()
+        {
+            PlaySound = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(PlaySound), false);
+            ThemeIsToggled = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(ThemeIsToggled), true);
+            FontSize = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(FontSize), 0.0);
+            CurrentPageOn = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(CurrentPageOn), "main");
+            ColorChoice = Microsoft.Maui.Storage.Preferences.Default.Get(nameof(ColorChoice), "Red");
+            UpdateSelectedColor(ColorChoice);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
