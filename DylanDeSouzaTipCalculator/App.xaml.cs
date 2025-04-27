aa4ac104de1fa0fde04f175897be6cfd3df3b959
Learner062022 <ddylan246@yahoo.com>
2025-04-27 21:14:26 +0800

    refactor(app): simplify OnStart by removing redundant method calls

--- DylanDeSouzaTipCalculator/App.xaml.cs
+++ DylanDeSouzaTipCalculator/App.xaml.cs
@@ -16,12 +16,6 @@ namespace DylanDeSouzaTipCalculator
             SyncfusionLicenseProvider.RegisterLicense(
                 "Ngo9BigBOggjHTQxAR8/V1NMaF1cXmhKYVFzWmFZfVtgdVdMZFxbRXZPMyBoS35Rc0VhW3xed3VSRWheV0F+");
             model.LoadPreferencesPageSettings();
-            model.LoadMainPagePreferences();
-            Current.UserAppTheme = model.ThemeIsToggled ? AppTheme.Dark : AppTheme.Light;
-            InitializeAudioPlayer();
         }
-
-        async void InitializeAudioPlayer() =>
-            await model.InitializeAudioPlayerAsync();
     }
 }
