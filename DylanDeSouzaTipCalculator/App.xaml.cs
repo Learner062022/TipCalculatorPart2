249ce3216273635606ab57856a94ce732c3c3f1f
Learner062022 <ddylan246@yahoo.com>
2025-04-30 16:38:21 +0800

    refactor(app): remove redundant model instantiation

--- DylanDeSouzaTipCalculator/App.xaml.cs
+++ DylanDeSouzaTipCalculator/App.xaml.cs
@@ -4,18 +4,13 @@ namespace DylanDeSouzaTipCalculator
 {
     public partial class App : Application
     {
-        readonly Model model = new();
-
         public App() => InitializeComponent();
 
         protected override Window CreateWindow(IActivationState? activationState)
             => new(new AppShell());
 
-        protected override void OnStart()
-        {
-            SyncfusionLicenseProvider.RegisterLicense(
+        protected override void OnStart() =>
+             SyncfusionLicenseProvider.RegisterLicense(
                 "Ngo9BigBOggjHTQxAR8/V1NMaF1cXmhKYVFzWmFZfVtgdVdMZFxbRXZPMyBoS35Rc0VhW3xed3VSRWheV0F+");
-            model.LoadPreferencesPageSettings();
-        }
     }
 }
